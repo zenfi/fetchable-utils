aeeb7b5eb269411b5e4565dbbf931138ba1ac18f
Manuel de la Torre <manuelmhtr@gmail.com>
2021-10-26 16:21:10 -0500

    Remove "load" event listener

--- banking/belvo-render-detector.js
+++ banking/belvo-render-detector.js
@@ -1,30 +1,28 @@
 (function () {
-  window.addEventListener("load", () => {
-    let renderedIframe = false;
+  let renderedIframe = false;
 
-    const checkRender = setInterval(() => {
-      const belvoContainer = document.getElementById("belvo");
+  const checkRender = setInterval(() => {
+    const belvoContainer = document.getElementById("belvo");
 
-      // Once iframe loaded, start checking content
-      if (belvoContainer.children.length > 0) {
-        if (!renderedIframe) {
-          renderedIframe = true;
-        } else {
-          const iframeDocument =
-            belvoContainer.children[0].contentWindow.document;
+    // Once iframe loaded, start checking content
+    if (belvoContainer.children.length > 0) {
+      if (!renderedIframe) {
+        renderedIframe = true;
+      } else {
+        const iframeDocument =
+          belvoContainer.children[0].contentWindow.document;
 
-          if (iframeDocument.getElementsByTagName("img").length > 0) {
-            clearInterval(checkRender);
-            window.ReactNativeWebView.postMessage("${MESSAGE}");
-          }
+        if (iframeDocument.getElementsByTagName("img").length > 0) {
+          clearInterval(checkRender);
+          window.ReactNativeWebView.postMessage("${MESSAGE}");
         }
       }
-    }, 10);
+    }
+  }, 10);
 
-    // 5 seconds fallback to render if render check fails
-    const fallback = setTimeout(() => {
-      clearInterval(checkRender);
-      window.ReactNativeWebView.postMessage("${MESSAGE}");
-    }, 5000);
-  });
+  // 5 seconds fallback to render if render check fails
+  const fallback = setTimeout(() => {
+    clearInterval(checkRender);
+    window.ReactNativeWebView.postMessage("${MESSAGE}");
+  }, 5000);
 })();

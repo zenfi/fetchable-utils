9ad1d947bb3f2e9d0f0af6a80b49dc6a4131ecb7
Manuel de la Torre <manuelmhtr@gmail.com>
2022-03-31 09:50:57 -0600

    Improvement: Temporary fix for Reconnecting view

--- banking/belvo-render-detector.js
+++ banking/belvo-render-detector.js
@@ -9,11 +9,11 @@
       if (!renderedIframe) {
         renderedIframe = true;
       } else {
-        const iframeDocument =
-          belvoContainer.children[0].contentWindow.document;
+        const iframeDocument = belvoContainer.children[0].contentWindow.document;
 
         if (iframeDocument.getElementsByTagName("img").length > 0) {
           clearInterval(checkRender);
+          displayReconnectScreen(iframeDocument);
           window.ReactNativeWebView.postMessage("${MESSAGE}");
         }
       }
@@ -26,3 +26,15 @@
     window.ReactNativeWebView.postMessage("${MESSAGE}");
   }, 5000);
 })();
+
+// TODO: Remove this function once bug is fixed by Belvo.
+// If a user tries to reconnect a bank account marked as invalid
+// (when password changed for example), the screen is not fully rendered
+// due to a problem with the "height" CSS property on ".credentials-form__content"
+// element. This function is a temporary fix for that.
+function displayReconnectScreen(iframeDocument) {
+  const container = iframeDocument.querySelector('.credentials-form__content');
+  if (container) {
+    container.style.height = 'auto';
+  }
+}
